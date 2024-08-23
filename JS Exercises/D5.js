5521e81091ceca745eb09e3ded544f1637fc33ff
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2024-08-23 16:17:00 +0200

    new

---
+++ JS Exercises/D5.js
@@ -0,0 +1,246 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+    Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
+*/
+const pets = ["dog", "cat", "hamster", "redfish"];
+
+console.log(pets);
+
+for (let i = 0; i < pets.length; i++) {
+  console.log(pets[i]);
+}
+
+/* ESERCIZIO 2
+    Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
+*/
+
+pets.sort();
+
+console.log(pets);
+
+/* ESERCIZIO 3
+    Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
+*/
+
+pets.reverse();
+
+console.log(pets);
+
+/* ESERCIZIO 4
+    Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
+*/
+
+let first = pets.shift();
+
+pets.push(first);
+
+console.log(pets);
+
+/* ESERCIZIO 5
+    Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
+*/
+const cars = [
+  {
+    brand: "Ford",
+    model: "Fiesta",
+    color: "red",
+    trims: ["titanium", "st", "active"],
+  },
+  {
+    brand: "Peugeot",
+    model: "208",
+    color: "blue",
+    trims: ["allure", "GT"],
+  },
+  {
+    brand: "Volkswagen",
+    model: "Polo",
+    color: "black",
+    trims: ["life", "style", "r-line"],
+  },
+];
+
+for (let i = 0; i < cars.length; i++) {
+  cars[i].licensePlate = true;
+}
+
+console.log(cars);
+
+/* ESERCIZIO 6
+    Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
+    Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
+*/
+
+let toyota = structuredClone(cars[0]);
+
+toyota.brand = "Toyota";
+toyota.model = "Supra";
+toyota.color = "white";
+
+console.log(toyota);
+cars.push(toyota);
+
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop();
+}
+
+console.log(cars);
+
+/* ESERCIZIO 7
+    Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
+*/
+const justTrims = [];
+
+for (let i = 0; i < cars.length; i++) {
+  justTrims.push(cars[i].trims[0]);
+}
+
+console.log(justTrims);
+
+/* ESERCIZIO 8
+    Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
+    "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
+*/
+
+for (let i = 0; i < cars.length; i++) {
+  if (cars[i].color.charAt(0) === "b") {
+    console.log(cars[i].brand, cars[i].color, ": Fizz");
+  } else {
+    console.log(cars[i].brand, cars[i].color, ": Buzz");
+  }
+}
+
+/* ESERCIZIO 9
+    Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
+*/
+const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105];
+
+let x = 0;
+
+while (numericArray[x] < 32 || numericArray[x] > 32) {
+  console.log(numericArray[x]);
+  x++;
+}
+
+/* ESERCIZIO 10
+    Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
+    dell'alfabeto italiano.
+    es. [f, b, e] --> [6, 2, 5]
+*/
+
+const charactersArray = ["g", "n", "u", "z", "d"];
+
+console.log("-----------------------------------------------");
+
+// Fatto con il for
+
+// const num = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// let lettertonum = [];
+
+// for (let i = 0; i < charactersArray.length; i++) {
+//   for (let j = 0; j < num.length; j++) {
+//     if (charactersArray[i] === num[j]) {
+//       lettertonum.push(num.indexOf(num[j]) + 1);
+//       break;
+//     }
+//   }
+// }
+// console.log(lettertonum);
+
+// fatto con switch
+
+let lettertonum = [];
+
+for (let i = 0; i < charactersArray.length; i++) {
+  switch (charactersArray[i]) {
+    case "a":
+      lettertonum.push(1);
+      break;
+    case "b":
+      lettertonum.push(2);
+      break;
+    case "c":
+      lettertonum.push(3);
+      break;
+    case "d":
+      lettertonum.push(4);
+      break;
+    case "e":
+      lettertonum.push(5);
+      break;
+    case "f":
+      lettertonum.push(6);
+      break;
+    case "g":
+      lettertonum.push(7);
+      break;
+    case "h":
+      lettertonum.push(8);
+      break;
+    case "i":
+      lettertonum.push(9);
+      break;
+    case "j":
+      lettertonum.push(10);
+      break;
+    case "k":
+      lettertonum.push(11);
+      break;
+    case "l":
+      lettertonum.push(12);
+      break;
+    case "m":
+      lettertonum.push(13);
+      break;
+    case "n":
+      lettertonum.push(14);
+      break;
+    case "o":
+      lettertonum.push(15);
+      break;
+    case "p":
+      lettertonum.push(16);
+      break;
+    case "q":
+      lettertonum.push(17);
+      break;
+    case "r":
+      lettertonum.push(18);
+      break;
+    case "s":
+      lettertonum.push(19);
+      break;
+    case "t":
+      lettertonum.push(20);
+      break;
+    case "u":
+      lettertonum.push(21);
+      break;
+    case "v":
+      lettertonum.push(22);
+      break;
+    case "w":
+      lettertonum.push(23);
+      break;
+    case "x":
+      lettertonum.push(24);
+      break;
+    case "y":
+      lettertonum.push(25);
+      break;
+    case "z":
+      lettertonum.push(26);
+      break;
+  }
+}
+
+console.log(lettertonum);
